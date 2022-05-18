3b3176c23d80733b4296f2ecdd4ea54707ef512a
Facundo Antivero <facu.antivero08@gmail.com>
2022-05-18 15:40:49 -0300

    Models del usuario finalizado

--- models/usuario.js
+++ models/usuario.js
@@ -1,1 +1,42 @@
 // Models de usuarios
+const { Schema, model } = require("mongoose");
+
+const UsuarioSchema = Schema({
+  nombre: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+  },
+  correo: {
+    type: String,
+    required: [true, "El correo es obligatorio"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "La contraseña es obligatoria"],
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    // enum: ["USER_ROLE", "ADMIN_ROLE"], no lo necesitamos ya que tenemos una colección en mongoose de roles.
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+UsuarioSchema.methods.toJSON = function () { // con esto hago que me retorne todos los datos, menos __v,password, y el resto de datos los guarda en usuario y los imprime
+  const { __v, password, _id, ...usuario } = this.toObject();
+  usuario.uid = _id;
+  return usuario;
+};
+
+module.exports = model("Usuario", UsuarioSchema);
