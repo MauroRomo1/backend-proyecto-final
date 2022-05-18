fcfd8d3d11337d9b1456cff5a18b5f14ebb6d617
Facundo Antivero <facu.antivero08@gmail.com>
2022-05-18 15:39:52 -0300

    Routes del usuario finalizado

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,1 +1,66 @@
 // usuarios
+const { Router } = require("express");
+const { check } = require("express-validator");
+
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const { tieneRole } = require("../middlewares/validar-roles");
+
+const {
+  esRoleValido,
+  emailExiste,
+  usuarioExiste,
+} = require("../helpers/db-validators");
+
+const {
+  usuariosGet,
+  usuarioPost,
+  usuarioPut,
+  usuarioDelete,
+} = require("../controllers/usuarios");
+
+const router = Router();
+
+router.get("/", usuariosGet);
+
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check(
+      "password",
+      "La contraseña debe tener como mínimo 6 caracteres"
+    ).isLength({ min: 6 }),
+    check("correo", "No es un correo válido").isEmail(), // solo me valida si el formato de email es valido
+    check("correo").custom(emailExiste), // me valida si  existe un email así en la BD
+    check("rol").custom(esRoleValido), // valida con una collection si existe un rol así 
+    validarCampos,
+  ],
+  usuarioPost
+);
+
+router.put(
+  "/:id",
+  [
+    validarJWT,
+    check("id", "No es un ID válido").isMongoId(),
+    check("id").custom(usuarioExiste),
+    check("rol").custom(esRoleValido),
+    validarCampos,
+  ],
+  usuarioPut
+);
+
+router.delete(
+  "/:id",
+  [
+    validarJWT,
+    tieneRole("ADMIN_ROLE", "VENTA_ROLE"),
+    check("id", "No es un ID válido").isMongoId(),
+    check("id").custom(usuarioExiste),
+    validarCampos,
+  ],
+  usuarioDelete
+);
+
+module.exports = router;
